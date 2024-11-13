5929c199cf3d3ca96dcf50b606c76e494e551d97
bileromerson <bileromerson@gmail.com>
2024-11-13 14:10:28 -0300

    main

--- python/conspiracy theory calculator/time_calculator.py
+++ python/conspiracy theory calculator/time_calculator.py
@@ -11,21 +11,19 @@ L = 0.0
 probabilidade = 95
 
 def calculo(n):
-    t = 2.5  # o tempo não preciza ser menor que 2.5
-
+    t = 3.0  # o tempo não preciza ser menor que 2.5
     phi = 1-math.pow((1 - 0.01),n*t)
     L = 1-math.pow(2.7182818, -t* phi)
+    
     if L < probabilidade/100:
-
         while L < probabilidade/100:
             t += 0.1
             phi = 1-math.pow((1 - 0.01),n*t)
             L = 1-math.pow(2.7182818, -t* phi)
-            print(L, t)
 
         if L >= probabilidade/100 and probabilidade/100 < 1.0:
-
             print(f"vai demorar: {round(t, 1)} ano(s) para: {round(L*100, 2)}%")
+
         else:
             print(f"vai demorar: {round(t, 1)} ano(s) para: {round(L*100, 2)-0.1}%")
 
