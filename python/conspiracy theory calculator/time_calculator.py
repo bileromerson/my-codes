5185f317bc52352d4d93819835fcff9bcc9daff2
bileromerson <bileromerson@gmail.com>
2024-11-13 13:38:10 -0300

    main

--- python/conspiracy theory calculator/time_calculator.py
+++ python/conspiracy theory calculator/time_calculator.py
@@ -3,31 +3,31 @@ import math
 '''
 Esta e uma brincadeira que fiz porque estava entediado, este código mostra os mesmos cálculos do inicial
 mas com a diferença que você não precisa especificar o número de anos, eu criei uma automação para que
-o código fale a melhor probabilidade no melhor período de tempo, que pode ser alterada pela variável "probabilidade",
-lembre-se que as probabilidades devem ser divididas por 100 para que o código funcione corretamente
+o código fale a melhor probabilidade no melhor período de tempo, que pode ser alterada pela variável "probabilidade".
 '''
 
 n = float(input("numero de conspiradores: "))
 L = 0.0
-probabilidade = 0.95
+probabilidade = 95
 
 def calculo(n):
-    t = 0.5
+    t = 2.5  # o tempo não preciza ser menor que 2.5
 
     phi = 1-math.pow((1 - 0.01),n*t)
     L = 1-math.pow(2.7182818, -t* phi)
+    if L < probabilidade/100:
 
-    if L < probabilidade:
-
-        while L < probabilidade:
-            
+        while L < probabilidade/100:
+            t += 0.1
             phi = 1-math.pow((1 - 0.01),n*t)
             L = 1-math.pow(2.7182818, -t* phi)
-            t += 0.05
+            print(L, t)
 
-            if L >= probabilidade:
+        if L >= probabilidade/100 and probabilidade/100 < 1.0:
 
-                print(f"vai demorar: {round(t, 2)} ano(s) para: {round(L*100, 2)}%")
+            print(f"vai demorar: {round(t, 1)} ano(s) para: {round(L*100, 2)}%")
+        else:
+            print(f"vai demorar: {round(t, 1)} ano(s) para: {round(L*100, 2)-0.1}%")
 
 
 calculo(n)
