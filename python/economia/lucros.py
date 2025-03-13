0ec657dabbedee2964ec3c93ab65d4c56397528c
bileromerson <158579506+bileromerson@users.noreply.github.com>
2025-03-13 16:27:14 -0300

    main

---
+++ python/economia/lucros.py
@@ -0,0 +1,24 @@
+num = int(input('=>>'))
+porcentagem = int(input('Digite a porcentagem de lucro: '))
+valor_inicial = float(input('Digite o valor inicial: '))
+meses = int(input('Digite a quantidade de meses: '))
+
+def total_lucro(porcentagem,meses,valor_inicial):
+
+    for i in range(1, int(meses) + 1):
+
+        valor_inicial = valor_inicial*(porcentagem/100)
+
+
+    print('O valor final é: ', round(valor_inicial, 2))
+
+def quanto_investir():
+    # continuar depois
+    # adicionarr for, para calcular o valor final dos meses.
+    v = valor_inicial (valor_inicial*(porcentagem/100))
+
+
+if num == 1:
+    total_lucro(porcentagem,meses,valor_inicial)
+else:
+    pass
